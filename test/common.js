c65d040a1436fc03cf6b20861d25755351d84f62
Tomas Aparicio <tomas@aparicio.me>
2015-08-29 11:36:51 +0100

    refactor: isolate modules

--- test/common.js
+++ test/common.js
@@ -3,6 +3,17 @@ const expect = require('chai').expect
 const common = require('../lib/common')
 
 suite('common', function () {
+  test('isRegExp', function () {
+    expect(common.isRegExp(/0-9/)).to.be.true
+    expect(common.isRegExp(new RegExp)).to.be.true
+    expect(common.isRegExp(null)).to.be.false
+    expect(common.isRegExp([])).to.be.false
+    expect(common.isRegExp({})).to.be.false
+    expect(common.isRegExp('')).to.be.false
+    expect(common.isRegExp(123)).to.be.false
+    expect(common.isRegExp(void 0)).to.be.false
+  })
+
   test('eachSeries', function (done) {
     var spy = sinon.spy()
     var arr = [ 1, 2, 3 ]
@@ -21,11 +32,11 @@ suite('common', function () {
     })
   })
 
-  test('sliceBuffer', function () {
+  test('splitBuffer', function () {
     var buf = []
     var buffer = new Buffer('Hello World')
 
-    common.sliceBuffer(1, buffer, 'utf8', buf)
+    common.splitBuffer(1, buffer, 'utf8', buf)
 
     expect(buf).to.have.length(11)
     expect(buf.shift()).to.be.deep.equal({
@@ -38,11 +49,11 @@ suite('common', function () {
     })
   })
 
-  test('sliceBuffer in lower chunk', function () {
+  test('splitBuffer in large chunks', function () {
     var buf = []
     var buffer = new Buffer('Hello World')
 
-    common.sliceBuffer(1024, buffer, 'utf8', buf)
+    common.splitBuffer(1024, buffer, 'utf8', buf)
 
     expect(buf).to.have.length(1)
     expect(buf.shift()).to.be.deep.equal({
