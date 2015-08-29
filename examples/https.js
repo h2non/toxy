7cb945bc49ab95ab3a8368f7cfe41cdeb976dc5b
Tomas Aparicio <tomas@aparicio.me>
2015-08-29 09:17:57 +0100

    refactor(example): https

--- examples/https.js
+++ examples/https.js
@@ -13,6 +13,15 @@ var opts = {
 
 var proxy = toxy(opts)
 
+// Forward to HTTPS server
+proxy
+  .get('/image/*')
+  .options({ secure: false })
+  .host('httpbin.org')
+  .forward('https://httpbin.org')
+  .poison(toxy.poisons.bandwidth({ bps: 1024 }))
+
+// Forward to plain HTTP server
 proxy
   .forward('http://httpbin.org')
   .poison(toxy.poisons.latency(1000))
@@ -21,3 +30,4 @@ proxy
 
 proxy.listen(3443)
 console.log('HTTPS server listening on port:', 3443)
+console.log('Test it: https://localhost:3443/image/jpeg')
